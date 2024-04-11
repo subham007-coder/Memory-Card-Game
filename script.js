4bacf462181fd32584c4e288962681f405d5e8eb
subham007-coder <dsubham109@gmail.com>
2024-04-11 14:40:20 +0530

    flip cardBox

--- script.js
+++ script.js
@@ -4,31 +4,25 @@ const startBtn = document.querySelector(".start");
 const moveCounter = document.querySelector(".moveCounter");
 const timeUpdate = document.querySelector(".timeUpdate");
 const reTry = document.querySelector(".retry");
+const cardInner = document.querySelectorAll(".card-inner");
+const main = document.querySelector("main");
 
+let flippedCards = [];
+let matchedCards = [];
 
-
-// start button
+// Start button
 startBtn.addEventListener("click", () => {
-  console.log("i am start");
   startBtn.style.display = "none";
   timeStart();
 });
 
-
-// retry game
-function reTryGame(params) {
-  reTry.addEventListener("click", () => {
-    time = 0;
-  })
-}
-reTryGame()
-
-
-
+// Retry game
+reTry.addEventListener("click", () => {
+  time = 0;
+});
 
 // Time update
 function timeStart() {
-
   setInterval(() => {
     time += 1;
     const minutes = Math.floor(time / 60);
@@ -38,17 +32,38 @@ function timeStart() {
   }, 1000);
 }
 
-
-
-
-
-
-
-
-// flip card
-
+// Flip card
 card.forEach((card) => {
   card.addEventListener("click", function () {
-    this.querySelector(".card-inner").classList.toggle("is-flipped");
+    if (flippedCards.length < 2 && !flippedCards.includes(card)) {
+      card.querySelector(".card-inner").classList.toggle("is-flipped");
+      flippedCards.push(card);
+      
+      if (flippedCards.length === 2) {
+        checkMatch();
+      }
+    }
   });
 });
+
+// Check if cards match
+function checkMatch() {
+  const [card1, card2] = flippedCards;
+  const id1 = card1.querySelector(".card-front").id;
+  const id2 = card2.querySelector(".card-front").id;
+  
+  if (id1 === id2) {
+    matchedCards.push(id1, id2);
+    if (matchedCards.length === card.length) {
+      // All cards matched, game over
+      console.log("Game Over!");
+    }
+  } else {
+    // Cards don't match, flip them back
+    setTimeout(() => {
+      card1.querySelector(".card-inner").classList.toggle("is-flipped");
+      card2.querySelector(".card-inner").classList.toggle("is-flipped");
+      flippedCards = [];
+    }, 1000);
+  }
+}
